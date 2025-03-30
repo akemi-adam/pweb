f8f282bd5926c5eb6b548ba4cd9db5c16305bc42
mosiah-adam <mosiah.adam@escolar.ifrn.edu.br>
2025-03-30 11:20:52 -0300

    Receita 16: Exercício 1

---
+++ receita16/app/page.js
@@ -0,0 +1,53 @@
+import Link from 'next/link'
+
+export default async function Home({searchParams}) {
+    const {titleSearchKey = 'bagdad', type = 'movie'} = await searchParams
+    const response = await fetch(`http://www.omdbapi.com/?apikey=${process.env.OMDB_API_KEY}&s=${titleSearchKey}&type=${type}`)
+    const data = await response.json()
+
+    return (
+        <div>
+            <div>
+                <h1>Viva Santana!</h1>
+                <h2>Lista de filmes</h2>
+                <ul>
+                    {data['Search'].map(movie => <li key={movie.imdbID}>{movie.Title} --- {movie.Year}</li>)}
+                </ul>
+                <ul>
+                    <li><Link href='/rota1'>Rota 1</Link></li>
+                    <li><Link href='/novarota'>Nova rota</Link></li>
+                    <li><Link href='/rota1/teste'>Sub rota da rota 1</Link></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export function Title({title}) {
+    return (
+        <h1>{title}</h1>
+    )
+}
+
+export function Table() {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Idade</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Akemi Adam</td>
+                    <td>20</td>
+                </tr>
+                <tr>
+                    <td>Isadora</td>
+                    <td>20</td>
+                </tr>
+            </tbody>
+        </table>
+    )
+}
