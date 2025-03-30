a3689b5c9b744b50cdddb3647cb0788a48e2d56a
mosiah-adam <mosiah.adam@escolar.ifrn.edu.br>
2025-03-30 09:56:21 -0300

    Receita 14: Exercício 1

---
+++ receita14/app/novarota/page.js
@@ -0,0 +1,13 @@
+import { MariaPrea } from './components'
+import DefaultList from './lists/DefaultList'
+
+
+export default function NovaRotaHome() {
+    return (
+        <div>
+            <h1>Nova Rota, Nova Página</h1>
+            <MariaPrea />
+            <DefaultList />
+        </div>
+    )
+}
