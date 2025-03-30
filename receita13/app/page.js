34d7e99b43c60dbc9f2f9fe27e554c292fcf7190
mosiah-adam <mosiah.adam@escolar.ifrn.edu.br>
2025-03-30 08:39:22 -0300

    Receita 13: Exercício 1

---
+++ receita13/app/page.js
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+        <div>
+            <div>Menu Principal</div>
+            <div>
+                <h1>Viva Santana!</h1>
+            </div>
+        </div>
+    )
+}
---
+++ receita13/app/rota1/page.js
@@ -0,0 +1,10 @@
+export default function FirstRoute() {
+    return (
+        <div>
+            <h1>Dúvida</h1>
+            <p>
+                Estou misturando inglês com português, o que é algo meio ruim, mas pior do que isso, não é ter que criar tantos arquivos com o mesmo nome? Cada rota que eu for fazer vai ter que se chamar page.js? Não acho bom, principalmente quando o projeto crescer muito.
+            </p>
+        </div>
+    )
+}
