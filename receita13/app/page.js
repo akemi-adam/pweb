43d8e383e3efb59f6abc8c1131321881bb643319
mosiah-adam <mosiah.adam@escolar.ifrn.edu.br>
2025-03-30 08:43:13 -0300

    Receita 13: Exercício 2

--- receita13/app/page.js
+++ receita13/app/page.js
@@ -8,3 +8,32 @@ export default function Home() {
         </div>
     )
 }
+
+export function Title({title}) {
+    return (
+        <h1>{title}</h1>
+    )
+}
+
+export function Table() {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Idade</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Akemi Adam</td>
+                    <td>20</td>
+                </tr>
+                <tr>
+                    <td>Isadora</td>
+                    <td>20</td>
+                </tr>
+            </tbody>
+        </table>
+    )
+}
