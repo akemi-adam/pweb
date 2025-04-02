b3d3ef4f3623def6f7132c5ffa34b56b3eed1fab
mosiah-adam <mosiah.adam@escolar.ifrn.edu.br>
2025-04-02 20:45:43 -0300

    Receita 20: Exercício 1

---
+++ receita20/app/movies/components.js
@@ -0,0 +1,52 @@
+import Form from 'next/form'
+import { useState } from 'react'
+
+
+export function MovieForm({action}) {
+    const [buttonStatus, setButtonStatus] = useState(false)
+
+    const types = [
+        {value: 'movie', label: 'Filme'},
+        {value: 'series', label: 'Série'},
+        {value: 'episode', label: 'Episódio'},
+    ]
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        setButtonStatus(true)
+        await action(new FormData(e.currentTarget))
+        setButtonStatus(false)
+    }
+
+    return (
+        <Form onSubmit={handleSubmit} className='flex flex-col gap-y-4'>
+            <div className='flex flex-col gap-y-2'>
+                <label htmlFor='idTitleSearchKey' className='text-xl'>Título</label>
+                <input name='titleSearchKey' id="titleSearchKey" className='border rounded-md p-1'/>
+                <label htmlFor='type' className='text-xl'>Tipo</label>
+                <select name='type' id='type' className='rounded-md border py-1'>
+                    {types.map((type, index) => <option value={type.value} key={index}>{type.label}</option>)}
+                </select>
+            </div>
+            <button type='submit' disabled={buttonStatus} className='border rounded-md bg-green-500 py-2 text-white hover:bg-white hover:text-green-500 hover:border-green-500 cursor-pointer'>
+                Pesquisar
+            </button>
+        </Form>
+    )
+}
+
+export function MovieList({movies}) {
+    return (
+        <div className='flex flex-col gap-y-10 overflow-y-scroll h-96'>
+            {movies.map(movie =>
+                <div key={movie.imdbID} className='flex flex-col gap-y-2'>
+                    <div className='flex justify-between text-xl'>
+                        <span>{movie.Title}</span>
+                        <span>{movie.Year}</span>
+                    </div>
+                    <img src={movie.Poster} alt={`${movie.Title} poster`} className='rounded-md' />
+                </div>
+            )}
+        </div>
+    )
+}
