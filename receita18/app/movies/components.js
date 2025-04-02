3cdee81c8fc34630f50555946db1cdd85b392dfc
mosiah-adam <mosiah.adam@escolar.ifrn.edu.br>
2025-04-02 09:02:59 -0300

    Receita 18: Desafio das Galáxias

--- receita18/app/movies/components.js
+++ receita18/app/movies/components.js
@@ -2,8 +2,9 @@ import Form from 'next/form'
 import { useState } from 'react'
 
 
-export function MovieForm({action}) {
-    const [buttonStatus, setButtonStatus] = useState(false)
+export function MovieForm({gambiarra}) {
+    // const [buttonStatus, setButtonStatus] = useState(false)
+    console.log('FORM')
 
     const types = [
         {value: 'movie', label: 'Filme'},
@@ -13,9 +14,9 @@ export function MovieForm({action}) {
 
     const handleSubmit = async (e) => {
         e.preventDefault()
-        setButtonStatus(true)
-        await action(new FormData(e.currentTarget))
-        setButtonStatus(false)
+        // setButtonStatus(true)
+        await gambiarra.action(new FormData(e.currentTarget))
+        // setButtonStatus(false)
     }
 
     return (
@@ -28,25 +29,44 @@ export function MovieForm({action}) {
                     {types.map((type, index) => <option value={type.value} key={index}>{type.label}</option>)}
                 </select>
             </div>
-            <button type='submit' disabled={buttonStatus} className='border rounded-md border-green-700 bg-green-200 py-2'>
+            <button type='submit' className='border rounded-md border-green-700 bg-green-200 py-2'>
                 Pesquisar
             </button>
         </Form>
     )
 }
 
-export function MovieList({movies}) {
+export function MovieList({gambiarra}) {
+    console.log('LISTA')
+    const [movies, setMovies] = useState([])
+    const [founded, setFounded] = useState(true)
+
+    gambiarra.action = async (formData) => {
+        try {
+            const response = await fetch(`http://www.omdbapi.com/?apikey=${process.env.NEXT_PUBLIC_OMDB_API_KEY}&s=${formData.get('titleSearchKey')}&type=${formData.get('type')}`)
+            const data = await response.json()
+            setMovies(data.Search || [])
+            setFounded(data.Response !== 'False')
+        } catch (error) {
+            console.log(error)
+            setFounded(false)
+        }
+    }
+
     return (
         <div className='flex flex-col gap-y-10 overflow-y-scroll h-96'>
-            {movies.map(movie =>
-                <div key={movie.imdbID} className='flex flex-col gap-y-2'>
-                    <div className='flex justify-between text-xl'>
-                        <span>{movie.Title}</span>
-                        <span>{movie.Year}</span>
+            {founded
+                ? movies.map(movie =>
+                    <div key={movie.imdbID} className='flex flex-col gap-y-2'>
+                        <div className='flex justify-between text-xl'>
+                            <span>{movie.Title}</span>
+                            <span>{movie.Year}</span>
+                        </div>
+                        <img src={movie.Poster} alt={`${movie.Title} poster`} className='rounded-md' />
                     </div>
-                    <img src={movie.Poster} alt={`${movie.Title} poster`} className='rounded-md' />
-                </div>
-            )}
+                )
+                : <div className='border rounded-md border-red-700 bg-red-200 p-2'>Nenhum filme encontrado!</div>
+            }
         </div>
     )
 }
--- receita18/app/movies/page.js
+++ receita18/app/movies/page.js
@@ -1,33 +1,24 @@
 'use client';
 
-import { useState } from 'react'
 import { MovieList, MovieForm } from './components'
 
 export default function Movies() {
-    const [movies, setMovies] = useState([])
-    const [founded, setFounded] = useState(true)
+    // const [founded, setFounded] = useState(true)
+    console.log('PÁGINA')
 
-    const action = async (formData) => {
-        try {
-            const response = await fetch(`http://www.omdbapi.com/?apikey=${process.env.NEXT_PUBLIC_OMDB_API_KEY}&s=${formData.get('titleSearchKey')}&type=${formData.get('type')}`)
-            const data = await response.json()
-            setMovies(data.Search || [])
-            setFounded(data.Response !== 'False')
-        } catch (error) {
-            console.log(error)
-            setFounded(false)
-        }
-    }
+    const gambiarra = {action: () => null}
 
     return (
         <div className='flex flex-col gap-y-8 mb-10'>
             <h2 className='text-center text-[2em] font-semibold pt-8'>Procurar filme</h2>
             <div className='w-96 flex flex-col gap-y-4 mx-auto'>
                 <div className='border rounded-md p-4'>
-                    <MovieForm action={action} />
+                    {/* <MovieForm action={action} /> */}
+                    <MovieForm gambiarra={gambiarra} />
                 </div>
                 <div className='border rounded-md p-4'>
-                    {founded ? <MovieList movies={movies} /> : <div className='border rounded-md border-red-700 bg-red-200 p-2'>Nenhum filme encontrado!</div>}
+                    <MovieList gambiarra={gambiarra}  />
+                    {/* {founded ? <MovieList gambiarra={gambiarra}  /> : <div className='border rounded-md border-red-700 bg-red-200 p-2'>Nenhum filme encontrado!</div>} */}
                 </div>
             </div>
         </div>
